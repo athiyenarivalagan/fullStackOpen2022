8c38ad2593ebef93dad7ada98c1a326a95994fe7
Athiyen <dhineesh2802@gmail.com>
2023-01-23 23:08:58 +0800

    completed part 3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Phonebook = require('./models/phonebook')
 
 
 app.use(express.static('build'))
-// parse the JSON string => convert JSON to JavaScript object 
+// parse the JSON string => convert JSON to JavaScript object
 app.use(express.json())
 app.use(cors())
 app.use(morgan(
@@ -18,56 +18,56 @@ app.use(morgan(
 // add a POST body content to all request & display it using the :post token
 morgan.token('post', function (req) {
   // JSON.stringify() converts a JavaScript value to a JSON string
-   return JSON.stringify(req.body)   
- })
+  return JSON.stringify(req.body)
+})
 
 
 app.get('/api/persons', (request, response) => {
   // an empty param{} fetches all objs
   Phonebook.find({})
-  .then(persons => {
+    .then(persons => {
     // toJSON() method returns a JSON string  of the result
-    response.json(persons)
-  })
+      response.json(persons)
+    })
 })
 
 
 app.get('/api/persons/:id', (request, response, next) => {
   Phonebook.findById(request.params.id)
-  .then(
-    person => {
-    if (person) { // person exits 
-      response.json(person) 
-    } else { 
-      /* if an id doesn't exist, the value of the
+    .then(
+      person => {
+        if (person) { // person exits
+          response.json(person)
+        } else {
+          /* if an id doesn't exist, the value of the
        person param will be null.
       */
-      response.status(404).end() // return id not found
-    }
-  })
+          response.status(404).end() // return id not found
+        }
+      })
   /* If the format of the id is incorrect (malformed), then we will
    end up in the error handler defined in the catch block.
   */
-  .catch(
-    error => next(error)) // passed onto the error handler
+    .catch(
+      error => next(error)) // passed onto the error handler
 })
 
 
-app.get('/info', (request, response) => { 
+app.get('/info', (request, response) => {
   Phonebook.find({})
-  .then(person => {
-    response.send(`<p>Phonebook has info for ${person.length} people
+    .then(person => {
+      response.send(`<p>Phonebook has info for ${person.length} people
     <p>${Date()}</p>`)
-  })
+    })
 })
 
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Phonebook.findByIdAndRemove(request.params.id)
-  .then(result => {
-    response.json(204).end()
-  })
-  .catch(error => next(error)) 
+    .then(() => {
+      response.json(204).end()
+    })
+    .catch(error => next(error))
 })
 
 
@@ -80,11 +80,11 @@ app.post('/api/persons', (request, response, next) => {
   })
 
   person.save()
-  .then(savedPerson => {
-    response.json(savedPerson)
-  })
-  .catch(error => next(error))
-  })
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
 
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -95,10 +95,10 @@ app.put('/api/persons/:id', (request, response, next) => {
     { name, number },
     { new: true, runValidators: true, context: 'query' }
   )
-  .then(updatedPerson => {
-    response.json(updatedPerson)
-  })
-  .catch(error => next(error))
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 
